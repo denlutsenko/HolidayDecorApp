3205ddd35612f8e5229fcf46b38ce83ca5726b34
den_lutsenko <coLLins1803!>
2019-08-06 09:14:19 +0300

    Postcards: create, update, delete flow

--- src/main/java/ua/com/hdcorp/hd/controller/PostcardController.java
+++ src/main/java/ua/com/hdcorp/hd/controller/PostcardController.java
@@ -1,15 +1,15 @@
 package ua.com.hdcorp.hd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.com.hdcorp.hd.model.Postcard;
 import ua.com.hdcorp.hd.service.PostcardService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "postcards")
@@ -23,7 +23,27 @@ public class PostcardController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Postcard>> getEmployees() {
+    public ResponseEntity<List<Postcard>> findPostcards() {
         return ResponseEntity.ok(postcardService.getPostcards());
+    }
+
+    @GetMapping(value = "/{postcardId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Postcard> findPostcard(@PathVariable("postcardId") Long postcardId) {
+        return ResponseEntity.ok(postcardService.findById(postcardId));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Postcard> createPostcard(@RequestBody Postcard postcard) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(postcardService.save(postcard));
+    }
+
+    @PatchMapping(value = "/{postcardId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Postcard> updatePostcard(@PathVariable("postcardId") Long postcardId, @RequestBody Map<String, String> postcardPatch) {
+        return ResponseEntity.status(HttpStatus.OK).body(postcardService.update(postcardId, postcardPatch));
+    }
+
+    @DeleteMapping(value = "/{postcardId}")
+    public ResponseEntity<Postcard> deletePostcard(@PathVariable("postcardId") Long postcardId) {
+        return ResponseEntity.status(HttpStatus.OK).body(postcardService.deactivate(postcardId));
     }
 }
