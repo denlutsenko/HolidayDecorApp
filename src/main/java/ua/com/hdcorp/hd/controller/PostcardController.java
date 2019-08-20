d6075fc957dc3e26b93cbf9772e6266f5acb7a87
lutsed01 <lutsed01@kits.kfplc.com>
2019-08-20 14:34:36 +0300

    HDCOR-19 create postCard

--- src/main/java/ua/com/hdcorp/hd/controller/PostcardController.java
+++ src/main/java/ua/com/hdcorp/hd/controller/PostcardController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ua.com.hdcorp.hd.model.Postcard;
 import ua.com.hdcorp.hd.service.PostcardService;
 
@@ -32,9 +33,11 @@ public class PostcardController {
         return ResponseEntity.ok(postcardService.findById(postcardId));
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Postcard> createPostcard(@RequestBody Postcard postcard) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(postcardService.save(postcard));
+    @PostMapping
+    public ResponseEntity<Postcard> createPostcard(@RequestParam(value = "file", required = false) MultipartFile file,
+                                                   @RequestParam(value = "vendorCode") String vendorCode,
+                                                   @RequestParam(value = "postcardTypeId") Long postcardTypeId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(postcardService.save(file, vendorCode, postcardTypeId));
     }
 
     @PatchMapping(value = "/{postcardId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
